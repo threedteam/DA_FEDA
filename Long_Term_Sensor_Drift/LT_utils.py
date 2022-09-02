bf4a07744421406d9bd7495e154be4f74f1df2e8
threedteam <49711050+threedteam@users.noreply.github.com>
2022-09-02 22:36:47 +0800

    Update LT_utils.py

--- Long_Term_Sensor_Drift/LT_utils.py
+++ Long_Term_Sensor_Drift/LT_utils.py
@@ -3,17 +3,16 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-from train import params
 from sklearn.manifold import TSNE
 
 def optimizer_scheduler(optimizer, p):
     """
-    Adjust the learning rate of optimizer 调整优化器的学习率
-    :param optimizer: optimizer for updating parameters  用于参数更新的优化器
-    :param p: a variable for adjusting learning rate     用于调整学习速率的变量
+    Adjust the learning rate of optimizer 
+    :param optimizer: optimizer for updating parameters 
+    :param p: a variable for adjusting learning rate     
     :return: optimizer
     """
     for param_group in optimizer.param_groups:
-        param_group['lr'] = 0.01 / (1. + 10 * p) ** 0.75
+        param_group['lr'] = 0.01 / (1. + 10 * p) ** 0.75 
 
     return optimizer
