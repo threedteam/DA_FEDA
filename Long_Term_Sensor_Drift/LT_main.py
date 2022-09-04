b9de5ffb4b5fb6f23b8c4ca91fe63d81e0949e8b
threedteam <49711050+threedteam@users.noreply.github.com>
2022-09-04 20:02:43 +0800

    Update LT_main.py

--- Long_Term_Sensor_Drift/LT_main.py
+++ Long_Term_Sensor_Drift/LT_main.py
@@ -5,13 +5,9 @@ import numpy as np
 from Long_Term_Sensor_Drift.LT_model import FeatureExtractor,class_classifier,Domain_classifier
 from Long_Term_Sensor_Drift.LT_loaddataset import train_loader,test_loader
 from Long_Term_Sensor_Drift import LT_train,LT_test,LT_params
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-plt.rc('font',family='Times New Roman')
 import torch.nn.functional as F
 import torch
 from torch.autograd import Variable
-from time import sleep
 import argparse,sys,os
 
 
@@ -22,28 +18,30 @@ def main(train_mode,FeatureExtractor,class_classifier,Domain_classifier,train_lo
     # BP_params.source_domain = args.source_domain
     # BP_params.target_domain = args.target_domain
 
-    src_train_dataloader = train_loader
-    src_test_dataloader = train_loader
-    tgt_test_dataloader = test_loader
-    tgt_trian_dataloader = test_loader
+    ### Since Github cannot upload code, the following section should be set according to the specific data and the specific data partition
+    ### Here is a demo I wrote to get the code up and running. Please reset where I marked (***) when using specific data
+    src_train_dataloader = train_loader   # Load the file based on the specific data  ***
+    src_test_dataloader = train_loader    # Load the file based on the specific data  ***
+    tgt_test_dataloader = test_loader     # Load the file based on the specific data  ***
+    tgt_trian_dataloader = test_loader    # Load the file based on the specific data  ***
 
     feature_extractor = FeatureExtractor
     class_classifier = class_classifier
     domain_classifier = Domain_classifier
 
-    class_criterion = nn.NLLLoss()
-    domain_criterion = nn.NLLLoss()
+    class_criterion = nn.NLLLoss()        # loss function
+    domain_criterion = nn.NLLLoss()       #
     # class_criterion = MultiCEFocalLoss(6)
 
 
-    optimizer = optim.SGD([{'params': feature_extractor.parameters()},
+    optimizer = optim.SGD([{'params': feature_extractor.parameters()},  # The optimizer
                            {'params': class_classifier.parameters()},
-                           {'params': domain_classifier.parameters()}], lr=BP_params.lr, momentum=0.9)
+                           {'params': domain_classifier.parameters()}], lr=LT_params.lr, momentum=0.9)
     for epoch in range(BP_params.epochs):
         print("Epoch: {}".format(epoch))
-        BP_train.train(train_mode,feature_extractor,class_classifier,domain_classifier,class_criterion,domain_criterion,
+        LT_train.train(train_mode,feature_extractor,class_classifier,domain_classifier,class_criterion,domain_criterion,
                        src_train_dataloader,tgt_trian_dataloader,optimizer,epoch)
-        accuracy = BP_test.test(feature_extractor,class_classifier,domain_classifier,src_test_dataloader,tgt_test_dataloader)
+        accuracy = LT_test.test(feature_extractor,class_classifier,domain_classifier,src_test_dataloader,tgt_test_dataloader)
         
         print("##################################")
         print(accuracy)
@@ -52,6 +50,6 @@ def main(train_mode,FeatureExtractor,class_classifier,Domain_classifier,train_lo
 if __name__ == '__main__':
     train_mode = "dann"
     feature_extractor,class_classifier,domain_classifier = FeatureExtractor(),class_classifier(),Domain_classifier()
-    train_loader,test_loader=train_loader,test_loader
+    train_loader,test_loader=train_loader,test_loader    ##  ***
     main(train_mode,feature_extractor,class_classifier,domain_classifier,train_loader,test_loader)
 
