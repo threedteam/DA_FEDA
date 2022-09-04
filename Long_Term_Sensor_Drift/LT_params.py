dd2f5bf6b3b75c33150d5e496b1a65434e908c2a
threedteam <49711050+threedteam@users.noreply.github.com>
2022-09-04 19:03:57 +0800

    Update LT_params.py

--- Long_Term_Sensor_Drift/LT_params.py
+++ Long_Term_Sensor_Drift/LT_params.py
@@ -1,16 +1,13 @@
-fig_mode = 'display'
-embed_plot_epoch=10
-
-lr = 0.01
-batch_size = 512
-epochs = 10
-gamma = 10
-theta = 1
-train_mode = "dann"
-source_domain = "source"
+lr = 0.01                  # You can modify this parameter value
+batch_size = 512           # You can modify this parameter value
+epochs = 10                # You can modify this parameter value
+gamma = 10                 # You can modify this parameter value
+theta = 1                  # You can modify this parameter value
+train_mode = "dann"        # Can be modified
+source_domain = "source"   
 target_domain = "target"
 
-weight_ring = 0.01
-weight_L2norm = 0.05
-redius = 25.0
-weight_entropy = 0.1
+weight_ring = 0.01         # You can modify this parameter value
+weight_L2norm = 0.05       # You can modify this parameter value
+redius = 25.0              # You can modify this parameter value, the fixed Z value
+weight_entropy = 0.1       # You can modify this parameter value
